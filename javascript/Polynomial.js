d7821e0439e2d8555aad201c35dfe3d3f8a8e4d4
Mohammad Yousefiyan <aa.yosefiyan7@gmail.com>
2022-11-03 21:38:47 +0330

    adding python code , coded with laya

--- javascript/Polynomial.js
+++ javascript/Polynomial.js
@@ -23,22 +23,30 @@ export class Polynomial {
         }
         return result;
     }
+    getValueAtUsingHorner(x) {
+        let deg = this.deg;
+        let result = this.#coEfficients[0];
+        for (let i = 1; i <= deg; i++) {
+            result = result * x + this.#coEfficients[i]
+        }
+        return result;
+    }
     get coEfficients() {
         return [...this.#coEfficients];
     }
     add(polynomial) {
         let coEffiecients = [];
-        for (let i = 0; i < Math.max(polynomial.deg , this.deg);i++){
+        for (let i = 0; i < Math.max(polynomial.deg, this.deg); i++) {
             coEffiecients.add(this.#coEfficients[i] + polynomial.coEfficients[i]);
         }
         return new Polynomial(coEffiecients);
     }
     substract(polynomial) {
         let coEffiecients = [];
-        for (let i = 0; i < Math.max(polynomial.deg , this.deg);i++){
+        for (let i = 0; i < Math.max(polynomial.deg, this.deg); i++) {
             coEffiecients.add(this.#coEfficients[i] - polynomial.coEfficients[i]);
         }
         return new Polynomial(coEffiecients);
     }
-    
+
 }
